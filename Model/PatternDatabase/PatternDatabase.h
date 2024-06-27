52bd338bab344cecb30f4ac51d49e434404ec8de
Ujjawal Agrawal <ujjawal13agrawal@gmail.com>
2024-06-27 17:51:10 +0530

    initial commit

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,15 @@
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
